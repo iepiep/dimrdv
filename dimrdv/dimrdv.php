1c4d484ea69dc174f3de83724200ec443d80845b
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-24 15:18:26 +0100

    Update dimrdv.php

--- dimrdv/dimrdv.php
+++ dimrdv/dimrdv.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author Roberto Minini <r.minini@solution61.fr>
  * @copyright 2025 Roberto Minini
@@ -13,60 +14,60 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class Dimrdv extends Module {
-
-    public function __construct() {
+class Dimrdv extends Module
+{
+    public function __construct()
+    {
         $this->name = 'dimrdv';
         $this->tab = 'shipping_logistics';
         $this->version = '1.0.0';
         $this->author = 'Roberto Minini';
         $this->need_instance = 0;
         $this->bootstrap = true;
-        $this->controllers = array('dimform');
+        $this->controllers = ['dimform'];
         $this->displayName = $this->l('DIM-RDV');
         $this->description = $this->l('Appointment management module with optimized itinerary.');
-        $this->ps_versions_compliancy = array('min' => '8.2.0', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = ['min' => '8.2.0', 'max' => _PS_VERSION_];
 
         parent::__construct();
     }
 
-public function isUsingNewTranslationSystem() {
-    return false;
-}
-    
-    public function install() {
-        if (!parent::install() ||
-                !$this->installSql() ||
-                !$this->registerHook('displayHome') ||
-                !$this->registerHook('actionFrontControllerSetMedia') ||
-                !$this->registerHook('displayBackOfficeHeader') ||
-                !$this->installTabs()
-        ) {
-            return false;
-        }
-        return true;
+    public function isUsingNewTranslationSystem()
+    {
+        return false;
     }
 
-    private function installTabs() {
-        // Onglet principal dans la barre latérale
+    public function install()
+    {
+        return parent::install()
+            && $this->installSql()
+            && $this->registerHook('displayHome')
+            && $this->registerHook('actionFrontControllerSetMedia')
+            && $this->registerHook('displayBackOfficeHeader')
+            && $this->installTabs();
+    }
+
+    private function installTabs()
+    {
         $parentTab = new Tab();
         $parentTab->active = 1;
         $parentTab->class_name = 'AdminDimrdvGestionRdv';
-        $parentTab->id_parent = 0; // 0 = Menu principal
+        $parentTab->id_parent = 0;
         $parentTab->module = $this->name;
-        $parentTab->icon = 'directions'; // Icône pour "Itinéraire"
+        $parentTab->icon = 'directions';
+
         foreach (Language::getLanguages(true) as $lang) {
             $parentTab->name[$lang['id_lang']] = 'DIM RDV';
         }
         $parentTab->add();
 
-        // Onglet de configuration sous "Configuration"
         $configTab = new Tab();
         $configTab->active = 1;
         $configTab->class_name = 'AdminDimrdvConfig';
-        $configTab->id_parent = (int) Tab::getIdFromClassName('AdminDimrdvGestionRdv'); // Parent = Gestion RDV
+        $configTab->id_parent = (int) Tab::getIdFromClassName('AdminDimrdvGestionRdv');
         $configTab->module = $this->name;
         $configTab->icon = 'settings';
+
         foreach (Language::getLanguages(true) as $lang) {
             $configTab->name[$lang['id_lang']] = 'Config';
         }
@@ -75,7 +76,8 @@ public function isUsingNewTranslationSystem() {
         return true;
     }
 
-    public function uninstallTab() {
+    public function uninstallTab()
+    {
         $tabs = ['AdminDimrdvGestionRdv', 'AdminDimrdvConfig'];
         foreach ($tabs as $tabClass) {
             $id_tab = (int) Tab::getIdFromClassName($tabClass);
@@ -87,92 +89,84 @@ public function isUsingNewTranslationSystem() {
         return true;
     }
 
-    private function installSql() {
+    private function installSql()
+    {
         $sql_file = dirname(__FILE__) . '/sql/installs.sql';
         if (!file_exists($sql_file)) {
             return false;
         }
-        $sql_content = file_get_contents($sql_file);
-        // Remplacement du préfixe de table
-        $sql_content = str_replace('PREFIX_', _DB_PREFIX_, $sql_content);
+        
+        $sql_content = str_replace('PREFIX_', _DB_PREFIX_, file_get_contents($sql_file));
         $queries = preg_split("/;\s*[\r\n]+/", $sql_content);
+        
         foreach ($queries as $query) {
-            if (!empty(trim($query))) {
-                if (Db::getInstance()->execute($query) == false) {
-                    return false;
-                }
+            if (!empty(trim($query)) && Db::getInstance()->execute($query) == false) {
+                return false;
             }
         }
         return true;
     }
 
-    private function uninstallSql() {
-        $sql = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'dim_rdv`';
-        if (Db::getInstance()->execute($sql) == false) {
-            return false;
-        }
-        return true;
+    private function uninstallSql()
+    {
+        return Db::getInstance()->execute('DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'dim_rdv`');
     }
 
-    public function uninstall() {
-        if (!parent::uninstall() ||
-                !$this->uninstallSql() ||
-                !$this->unregisterHook('displayHome') ||
-                !$this->unregisterHook('actionFrontControllerSetMedia') ||
-                !$this->unregisterHook('displayBackOfficeHeader') ||
-                !$this->uninstallTab()
-        ) {
-            return false;
-        }
-        return true;
+    public function uninstall()
+    {
+        return parent::uninstall()
+            && $this->uninstallSql()
+            && $this->unregisterHook('displayHome')
+            && $this->unregisterHook('actionFrontControllerSetMedia')
+            && $this->unregisterHook('displayBackOfficeHeader')
+            && $this->uninstallTab();
     }
 
-    // Méthode de réinitialisation : efface uniquement les données de la table
-    public function resetModuleData() {
-        $sql = 'TRUNCATE TABLE `' . _DB_PREFIX_ . 'dim_rdv`';
-        return Db::getInstance()->execute($sql);
+    public function resetModuleData()
+    {
+        return Db::getInstance()->execute('TRUNCATE TABLE `' . _DB_PREFIX_ . 'dim_rdv`');
     }
 
-    // Back office : page de configuration du module
-    public function getContent() {
+    public function getContent()
+    {
         if (Tab::getIdFromClassName('AdminDimrdvConfig')) {
             Tools::redirectAdmin($this->context->link->getAdminLink('AdminDimrdvConfig'));
-        } else {
-            return $this->displayError($this->l('Erreur : L’onglet de configuration n’est pas installé.'));
         }
+        return $this->displayError($this->l('Erreur : L’onglet de configuration n’est pas installé.'));
     }
 
-    // Hook affiché en front office sur la page d’accueil
-    public function hookDisplayHome($params) {
-        $this->context->smarty->assign(array(
-            'dimrdv_link' => $this->context->link->getModuleLink($this->name, 'dimform', array(), true),
-            'module' => $this, // Assigner l'instance du module pour utiliser $module->l()
-        ));
+    public function hookDisplayHome($params)
+    {
+        $this->context->smarty->assign([
+            'dimrdv_link' => $this->context->link->getModuleLink($this->name, 'dimform', [], true),
+            'module' => $this,
+        ]);
         return $this->display(__FILE__, 'views/templates/hook/displayHome.tpl');
     }
 
-    // Ajout de CSS/JS si nécessaire
-public function hookActionFrontControllerSetMedia($params)
-{
-    $this->context->controller->registerStylesheet(
-        'dimrdv-css',
-        $this->_path . 'views/css/front/dimrdv.css',
-        ['media' => 'all', 'priority' => 150]
-    );
-    $this->context->controller->registerJavascript(
-        'dimrdv-js',
-        $this->_path . 'views/js/front/dimrdv.js',
-        ['position' => 'bottom', 'priority' => 150]
-    );
-}
+    public function hookActionFrontControllerSetMedia($params)
+    {
+        $this->context->controller->registerStylesheet(
+            'dimrdv-css',
+            $this->_path . 'views/css/front/dimrdv.css',
+            ['media' => 'all', 'priority' => 150]
+        );
+        
+        $this->context->controller->registerJavascript(
+            'dimrdv-js',
+            $this->_path . 'views/js/front/dimrdv.js',
+            ['position' => 'bottom', 'priority' => 150]
+        );
+    }
 
-
-    public function hookdisplayBackOfficeHeader($params) {
+    public function hookDisplayBackOfficeHeader($params)
+    {
         $this->context->controller->registerStylesheet(
             'dimrdv-css',
             $this->_path . 'views/css/back/dimrdv.css',
             ['media' => 'all', 'priority' => 150]
         );
+        
         $this->context->controller->registerJavascript(
             'dimrdv-js',
             $this->_path . 'views/js/back/dimrdv.js',
