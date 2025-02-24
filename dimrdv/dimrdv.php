dd9bbf6d4141fcb005ced9a727974933559a79ce
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-24 11:07:04 +0100

    Update dimrdv.php

--- dimrdv/dimrdv.php
+++ dimrdv/dimrdv.php
@@ -1,12 +1,13 @@
 <?php
-
 /**
  * This file is part of the dimrdv project.
- * 
+ *
  * (c) 2025 iepiep <r.minini@solution61.fr>
- * 
+ *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
+ *
+ * @author Roberto Minini <r.minini@solution61.fr>
  */
 
 if (!defined('_PS_VERSION_')) {
