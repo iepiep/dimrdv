de795e3d62211c64af504b865079c9c7fb360e53
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-24 11:12:36 +0100

    Update dimform.php

--- dimrdv/controllers/front/dimform.php
+++ dimrdv/controllers/front/dimform.php
@@ -2,12 +2,12 @@
 /**
  * This file is part of the dimrdv project.
  *
- * (c) 2025 iepiep <r.minini@solution61.fr>
+  * @copyright 2025 Roberto Minini
+ * @author Roberto Minini <r.minini@solution61.fr>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  *
- * @author Roberto Minini <r.minini@solution61.fr>
  */
 
 if (!defined('_PS_VERSION_')) { exit; }
