f664306b5a99fa548fd5bf8ae3e3cd3b168eb44d
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-24 16:19:13 +0100

    Update AdminDimrdvConfigController.php

--- dimrdv/controllers/admin/AdminDimrdvConfigController.php
+++ dimrdv/controllers/admin/AdminDimrdvConfigController.php
@@ -14,11 +14,12 @@ if (!defined('_PS_VERSION_')) {
 
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 
-class AdminDimrdvConfigController extends FrameworkBundleAdminController {
-
-    public function indexAction() {
+class AdminDimrdvConfigController extends FrameworkBundleAdminController
+{
+    public function indexAction()
+    {
         return $this->render('@Modules/dimrdv/views/templates/admin/configure.html.twig', [
-                    'module_link' => $this->generateUrl('admin_dimrdv_config'),
+            'module_link' => $this->generateUrl('admin_dimrdv_config'),
         ]);
     }
 }
