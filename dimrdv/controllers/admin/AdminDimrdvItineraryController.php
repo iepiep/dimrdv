d1c42090df6531fcbe838cbd654b08089b8098ce
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-24 17:39:52 +0100

    Update AdminDimrdvItineraryController.php

--- dimrdv/controllers/admin/AdminDimrdvItineraryController.php
+++ dimrdv/controllers/admin/AdminDimrdvItineraryController.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * @author Roberto Minini <r.minini@solution61.fr>
  * @copyright 2025 Roberto Minini
@@ -8,7 +7,6 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
