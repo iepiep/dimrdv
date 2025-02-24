00ff50850706fc13ec884fe4b1712e664aae2dbb
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-24 15:34:00 +0100

    Update AdminDimrdvGestionRdvController.php

--- dimrdv/controllers/admin/AdminDimrdvGestionRdvController.php
+++ dimrdv/controllers/admin/AdminDimrdvGestionRdvController.php
@@ -8,23 +8,26 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class AdminDimrdvGestionRdvController extends ModuleAdminController {
-
-    public function __construct() {
+class AdminDimrdvGestionRdvController extends ModuleAdminController
+{
+    public function __construct()
+    {
         $this->bootstrap = true; // Pour utiliser le style Bootstrap
         parent::__construct();
     }
 
-    public function initContent() {
+    public function initContent()
+    {
         parent::initContent();
 
         // Récupérer la liste des RDV depuis la table
-        $rdvs = Db::getInstance()->executeS('SELECT * FROM `' . _DB_PREFIX_ . 'dim_rdv` ORDER BY date_creneau1 ASC') ?: [];
+        $rdvs = Db::getInstance()->executeS(
+            'SELECT * FROM `' . _DB_PREFIX_ . 'dim_rdv` ORDER BY date_creneau1 ASC'
+        ) ?: [];
 
         $this->context->smarty->assign([
             'rdvs' => $rdvs,
@@ -33,10 +36,9 @@ class AdminDimrdvGestionRdvController extends ModuleAdminController {
         // Vérifie le chemin d'accès du template
         $templatePath = _PS_MODULE_DIR_ . 'dimrdv/views/templates/admin/gestionRdv.tpl';
         if (!file_exists($templatePath)) {
-            die("Le template '$templatePath' est introuvable.");
+            exit("Le template '$templatePath' est introuvable.");
         }
 
         $this->setTemplate('module:dimrdv/views/templates/admin/gestionRdv.tpl');
     }
-
 }
