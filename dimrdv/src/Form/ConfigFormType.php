a7c6e9e085f74c89db80c5d393070f902bc56537
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-26 11:58:51 +0100

    Update ConfigFormType.php

--- dimrdv/src/Form/ConfigFormType.php
+++ dimrdv/src/Form/ConfigFormType.php
@@ -8,9 +8,12 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
 namespace Iepiep\Dimrdv\Form;
 
-if (!defined('_PS_VERSION_')) { exit; }
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
