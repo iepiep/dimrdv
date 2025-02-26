723313ff7a7d500506e8e95581842411a1bc4aa7
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-26 09:57:15 +0100

    Update AdminDimrdvConfigController.php

--- dimrdv/controllers/admin/AdminDimrdvConfigController.php
+++ dimrdv/controllers/admin/AdminDimrdvConfigController.php
@@ -8,18 +8,44 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
-if (!defined('_PS_VERSION_')) {
-    exit;
-}
+namespace  Iepiep\Dimrdv\Controller\Admin;
 
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminDimrdvConfigController extends FrameworkBundleAdminController
 {
-    public function indexAction()
+    public function indexAction(Request $request): Response
     {
-        return $this->render('@Modules/dimrdv/views/templates/admin/configure.html.twig', [
-            'module_link' => $this->generateUrl('admin_dimrdv_config'),
+        $form = $this->createForm('Iepiep\Dimrdv\Form\ConfigFormType', [
+            'DIMRDV_GOOGLE_API_KEY' => $this->getConfigurationValue('DIMRDV_GOOGLE_API_KEY'),
         ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $this->updateConfigurationValue('DIMRDV_GOOGLE_API_KEY', $data['DIMRDV_GOOGLE_API_KEY']);
+
+            $this->addFlash('success', $this->trans('Settings updated.', 'Admin.Notifications.Success'));
+
+            return $this->redirectToRoute('admin_dimrdv_config');
+        }
+
+        return $this->render('@Modules/dimrdv/views/templates/admin/configure.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    private function getConfigurationValue(string $key): string
+    {
+        return \Configuration::get($key) ?: '';
+    }
+
+    private function updateConfigurationValue(string $key, string $value): void
+    {
+        \Configuration::updateValue($key, $value);
     }
 }
