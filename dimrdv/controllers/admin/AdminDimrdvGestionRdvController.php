aeb57749f857f7ef6e61b675f167d349722f35b2
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-02-26 11:57:48 +0100

    Update AdminDimrdvGestionRdvController.php

--- dimrdv/controllers/admin/AdminDimrdvGestionRdvController.php
+++ dimrdv/controllers/admin/AdminDimrdvGestionRdvController.php
@@ -8,9 +8,12 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
 namespace  Iepiep\Dimrdv\Controller\Admin;
 
-if (!defined('_PS_VERSION_')) { exit; }
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\HttpFoundation\Response;
